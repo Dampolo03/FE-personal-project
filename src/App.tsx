153d7e80ced4a28d44494fcb2b877c91a6e8d337
dampolo03 <shobowaledamilolapaul@gmail.com>
2022-01-16 16:19:15 +0100

    added app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.scss";
+import { RouterComponent } from "./router";
 
 function App() {
   return (
     <>
-      <div>App</div>
+      <RouterComponent />
     </>
   );
 }
